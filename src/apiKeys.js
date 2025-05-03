d3ae94e7214ff9cb05324d57247556717e6af708
Karina Shirke <karinashirke503@gmail.com>
2025-05-03 15:44:54 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1f70cc3c3434f1d3aabb9a4530d40b62",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
